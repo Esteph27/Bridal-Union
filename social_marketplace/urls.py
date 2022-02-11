8e98325e84be9ea853c3d554b893ce420f328637
Esteph27 <esteph.csa@gmail.com>
2022-02-11 18:40:53 +0000

    Wire up remaining templates and urls

--- social_marketplace/urls.py
+++ social_marketplace/urls.py
@@ -1,12 +1,17 @@
-from . import views
-from django.urls import path
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
 from django.conf import settings
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('', views.ViewHome.as_view(), name='home'),
-    path('designer/', views.ViewDesigner.as_view(), name='post_detail'),
+    path('discover-designer/', views.ViewDiscoverDesigners.as_view(), name='post_detail'),
+    path('designer/', views.ViewDesigner.as_view(), name='view_designers'),
+    path('designer-portfolio/', views.ViewDesignerPortfolio.as_view(), name='view_portfolio'),
+    path('booking/', views.ViewBooking.as_view(), name='view_booking'),
+    path('login/', views.ViewLogin.as_view(), name='login'),
+    path('customer-account/', views.ViewCustomerAccount.as_view(), name='view_account'),
 ]
--- social_marketplace/views.py
+++ social_marketplace/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic.base import TemplateView
+# -----------> models imported
 from .models import Designer
 
 
 class ViewHome(TemplateView):
+    '''render home page template'''
 
     template_name = 'index.html'
 
 
+class ViewDiscoverDesigners(TemplateView):
+    '''render home discover designers template'''
+
+    template_name = 'discover_designers.html'
+
+
 class ViewDesigner(generic.ListView):
+    '''render designer page template and designer model'''
 
     model = Designer
     template_name = 'designer.html'
+
+
+class ViewDesignerPortfolio(TemplateView):
+    '''render designer portfolio template'''
+
+    template_name = 'designer_portfolio.html'
+
+
+class ViewBooking(TemplateView):
+    '''render booking page template'''
+
+    template_name = 'booking.html'
+
+
+class ViewLogin(TemplateView):
+    '''render home page template'''
+
+    template_name = 'login.html'
+
+
+class ViewCustomerAccount(TemplateView):
+    '''render customer account template'''
+
+    template_name = 'customer_account.html'
