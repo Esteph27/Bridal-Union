4fa00b0977e9c66380c7251749ae941929f9b223
Esteph27 <esteph.csa@gmail.com>
2022-02-11 17:47:04 +0000

    Wire up first templates and add favicon

--- social_marketplace/urls.py
+++ social_marketplace/urls.py
@@ -1,8 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('', views.ViewHome.as_view(), name='home'),
     path('designer/', views.ViewDesigner.as_view(), name='post_detail'),
 ]
