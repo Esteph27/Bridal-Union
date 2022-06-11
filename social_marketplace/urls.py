3e911a7616c09855ea6ecd603293d015bf3c1496
Esteph27 <esteph.csa@gmail.com>
2022-06-11 18:07:36 +0000

    test - fix view profile 404 error for new users

--- social_marketplace/urls.py
+++ social_marketplace/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('', views.ViewHome.as_view(), name='home'),
     path('about/', views.ViewAbout.as_view(), name='about'),
+    # designers 
     path('discover_designers/', views.ViewDiscoverDesigners.as_view(), name='discover_designers'),
     path('<slug:slug>/', views.ImagePostDetail.as_view(), name='imagepost_detail'),
     path('like/<slug:slug>/', views.ImagePostLike.as_view(), name='imagepost_like'),
     path('designer_profile/<pk>/', views.ViewDesigner.as_view(), name='view_designer_profile'),
+    # user account
     path('customer_account/', views.ViewCustomerAccount.as_view(), name='view_account'),
 ]
